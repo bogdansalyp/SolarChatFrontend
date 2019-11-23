a7a2fff5c4bc89d8739530946a49779d8e9f6311
Nell <ADshishova@mail.ru>
2019-11-23 16:35:55 +0300

    fixed onmessage

--- src/Chat/Chat.ts
+++ src/Chat/Chat.ts
@@ -12,6 +12,11 @@ export default class BoardChangeView {
 
     render() {
         this.el.innerHTML += ChatTemplate();
+        
+        const messageField = document.getElementById('dialogview-page');
+        const createMessageForm = <HTMLFormElement> document.getElementById('createMessageData');
+        const newMessage = new MessageComponent(messageField);
+
         (<any>window).socket = new WebSocket(BACKEND_ADDRESS + '/chat');
 
         (<any>window).socket.onopen = function(result) {
@@ -21,32 +26,27 @@ export default class BoardChangeView {
             }
         };
 
-        // (<any>window).socket.onclose = function(event) {
-        // if (event.wasClean) {
-        //     console.log('cоединение закрыто');
-        // } else {
-        //     console.log('соединения как-то закрыто');
-        // }
-        // };
+        (<any>window).socket.onclose = function(event) {
+        if (event.wasClean) {
+            console.log('cоединение закрыто');
+        } else {
+            console.log('соединения как-то закрыто');
+        }
+        };
         
         (<any>window).socket.onmessage = function(event) {
             console.log("пришли данные " + event.data);
+            newMessage.render({messageAuthor: 'Username', messageContent: event.data});
         };
         
         (<any>window).socket.onerror = function(event) {
             console.log("ошибка " + event.message);
         };
 
-        (<any>window).socket.onmessage = function(result) {
-        };
-
-        const messageField = document.getElementById('dialogview-page');
-        const createMessageForm = <HTMLFormElement> document.getElementById('createMessageData');
         createMessageForm.addEventListener('submit', (e) => {
             e.preventDefault();
     
             const message = createMessageForm.elements['message'].value;
-            const newMessage = new MessageComponent(messageField);
 
             if (message != '') {
                 (<any>window).socket.send(JSON.stringify({id_sender: 5, username_recipient: 'ADshishova', text: 'JHJJJJJJ'}));
--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_ADDRESS = 'ws://solarsunrise.ru:8080';
-// export const BACKEND_ADDRESS = 'ws://localhost:8080';
+// export const BACKEND_ADDRESS = 'ws://solarsunrise.ru:8080';
+export const BACKEND_ADDRESS = 'ws://localhost:8090';
