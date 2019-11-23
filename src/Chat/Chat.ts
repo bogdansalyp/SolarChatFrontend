c5b9f0d0cd3f87a0adee69d0f48ddcc1bf7243af
Nell <ADshishova@mail.ru>
2019-11-23 13:36:40 +0300

    added messages

--- src/Chat/Chat.ts
+++ src/Chat/Chat.ts
@@ -1,19 +1,19 @@
 import ChatTemplate from './Chat.hbs';
 import './Chat.scss';
 import {BACKEND_ADDRESS} from '../config/Config';
+import MessageComponent from '../components/Message/Message';
 
 export default class BoardChangeView {
     el: HTMLElement;
 
     constructor(el) {
         this.el = el;
-
-        (<any>window).socket = new WebSocket(BACKEND_ADDRESS + '/chat');
     }
 
     render() {
         this.el.innerHTML += ChatTemplate();
-        
+        (<any>window).socket = new WebSocket(BACKEND_ADDRESS + '/chat');
+
         (<any>window).socket.onopen = function(result) {
             for (let i = 0; i < 3; i++) {
                 (<any>window).socket.send(JSON.stringify({id_sender: 5, username_recipient: 'ADshishova', text: 'Hello, ADshishova!'}));
@@ -22,5 +22,19 @@ export default class BoardChangeView {
 
         (<any>window).socket.onmessage = function(result) {
         };
+
+        const messageField = document.getElementById('dialogview-page');
+        const createMessageForm = <HTMLFormElement> document.getElementById('createMessageData');
+        createMessageForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+    
+            const message = createMessageForm.elements['message'].value;
+            const newMessage = new MessageComponent(messageField);
+            newMessage.render({messageAuthor: 'Username', messageContent: message});
+
+            (<any>window).socket.send(JSON.stringify({id_sender: 5, username_recipient: 'ADshishova', text: 'JHJJJJJJ'}));
+    
+            createMessageForm.elements['message'].value = '';
+        });
     }
 }
