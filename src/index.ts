10a7aa0967771659def3bb804cd02411fd35be90
Bogdan Salyp <bogdansalyp@gmail.com>
2019-11-23 14:57:51 +0300

    Added cookie

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Chat from './Chat/Chat';
 
+document.cookie = "username=" + window.location.search.substr(1).split('=')[1];
+
 let el = document.getElementById('application');
 (new Chat(el)).render();
